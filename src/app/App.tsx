f06e3226e14c8ba6673202a30e7f61814ac0f291
project-88288 <chaiya.88288@gmail.com>
2022-12-20 07:54:47 +0700

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,8 @@
 import { getErrorMessage } from "utils/error"
-import Layout, { Page } from "components/layout"
-import { Banner, Content, Header, Actions } from "components/layout"
-import { ErrorBoundary, Wrong } from "components/feedback"
 
 /* routes */
 import { useNav } from "./routes"
-
-/* banner */
-import NetworkName from "./sections/NetworkName"
-
-/* sidebar */
-// import Nav from "./sections/Nav"
-// import Aside from "./sections/Aside"
-
-/* header */
-import IsClassicNetwork from "./sections/IsClassicNetwork"
-import Refresh from "./sections/Refresh"
-import Preferences from "./sections/Preferences"
-import SelectTheme from "./sections/SelectTheme"
-import ConnectWallet from "./sections/ConnectWallet"
-
-/* extra */
-import LatestTx from "./sections/LatestTx"
-import ValidatorButton from "./sections/ValidatorButton"
-import DevTools from "./sections/DevTools"
+import { ErrorBoundary, Wrong } from "components/feedback"
 
 /* init */
 import InitBankBalance from "./InitBankBalance"
@@ -32,33 +11,9 @@ const App = () => {
   const { element: routes } = useNav()
 
   return (
-    <Layout>
-      <Banner>
-        <NetworkName />
-      </Banner>
-
-      <Header>
-        <IsClassicNetwork />
-
-        <Actions>
-          <DevTools />
-          <section>
-            <Refresh />
-            <Preferences />
-            <SelectTheme />
-          </section>
-          <ValidatorButton />
-          <ConnectWallet />
-        </Actions>
-        <LatestTx />
-      </Header>
-
-      <Content>
-        <ErrorBoundary fallback={fallback}>
-          <InitBankBalance>{routes}</InitBankBalance>
-        </ErrorBoundary>
-      </Content>
-    </Layout>
+    <ErrorBoundary fallback={fallback}>
+      <InitBankBalance>{routes}</InitBankBalance>
+    </ErrorBoundary>
   )
 }
 
@@ -66,7 +21,5 @@ export default App
 
 /* error */
 export const fallback = (error: Error) => (
-  <Page>
-    <Wrong>{getErrorMessage(error)}</Wrong>
-  </Page>
+  <Wrong>{getErrorMessage(error)}</Wrong>
 )
